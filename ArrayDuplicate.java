4267a7a6f7722abaf0aeb12c71689a95ef95f7f1
AT <athirapratheep2002@gmail.com>
2025-01-16 22:09:36 +0530

    Create ArrayDuplicate.java
    Finding duplicates of an array using hashmap and arraylist

---
+++ ArrayDuplicate.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class ArrayDuplicate{
+    static List<Integer> findDuplicates(Integer[] arr)
+    {
+        Map<Integer,Integer> hmap=new HashMap<>();
+        List<Integer> duplicates=new ArrayList<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            hmap.put(arr[i],hmap.getOrDefault(arr[i],0)+1);
+        }
+        // int count=0;
+        for(Map.Entry<Integer,Integer> entry: hmap.entrySet())
+        {
+            if(entry.getValue() > 1)
+            {
+                duplicates.add(entry.getKey());
+            }
+        }
+        System.out.println("Count of duplicates respectively:"+ hmap);
+        if (duplicates.isEmpty()) {
+            duplicates.add(-1);
+        }
+        return duplicates;
+    }       
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        Integer[] arr=new Integer[n];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        List<Integer> result=findDuplicates(arr);
+        System.out.println("The duplicates are:");
+        for(int i:result)
+        {
+            System.out.println(i+" ");
+        }
+        sc.close();
+    }
+}
