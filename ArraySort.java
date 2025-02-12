23ce8778d16d1dd03dde957c17172e036ff345e2
AT <athirapratheep2002@gmail.com>
2025-02-12 23:16:36 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -8,7 +8,7 @@ public class ArraySort {
         for(int i=0;i<arr.length;i++)
         {
             arr[i]=sc.nextInt();
-        }kbkbnnl
+        }
         for(int i=0;i<arr.length-1;i++)
         {
             if(arr[i]<=arr[i+1])
