8281fa32c755a74be5c35cf4047c8175a031caa0
AT <athirapratheep2002@gmail.com>
2025-02-24 21:41:35 +0530

    Update ArrayRotation.java

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -5,6 +5,7 @@ public class ArrayRotation {
         int n=arr.length;
         for(int i=0;i<d;i++)
         {
+            m
             int first=arr[0];
             for(int j=0;j<n-1;j++) 
             {
