f5879fa93e51f41e558510fa2d1c97cccbee8fc4
AT <athirapratheep2002@gmail.com>
2025-03-02 19:18:20 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -6,6 +6,7 @@ public class ArraySort {
         int size=sc.nextInt();
         int arr[]=new int[size];
         for(int i=0;i<arr.length;i++)
+            
         {
             arr[i]=sc.nextInt();
         }
