d13595b61a952456519c98eeb3db6f95feff52fd
AT <athirapratheep2002@gmail.com>
2025-07-14 19:51:48 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -2,27 +2,30 @@ import java.util.Scanner;
 public class ArraySort {
     public static void main(String[] args)
     {
-        Scanner sc= new Scanner(System.in);
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        for(int i=0;i<arr.length;i++)
-            
-        {
-            arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        for(int i=0;i<arr.length-1;i++)
-        {
-            if(arr[i]<=arr[i+1])
-            {
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
+        
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
         }
-        for(int i=arr.length-1;i>=0;i--)
-        {
-            System.out.print(arr[i]+" ");
+
+        for(int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
         }
+        
         sc.close();
     }
 }
